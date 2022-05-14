096251116ba6958f89f35d127e0584bad6ab6e76
maticabsec <matic.absec@student.um.si>
2022-05-14 19:22:03 +0200

    commit 2.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavB from './components/nav';
-import { BrowserRouter,Routes, Route,} from "react-router-dom";
+import {Routes, Route,} from "react-router-dom";
 import PrvaStran from './components/prvastran';
 
-import { createRoot } from "react-dom/client";
 
 function App() {
   return (
-    <BrowserRouter>
- 
-
       <Routes>
         <Route path='/prva' element={<PrvaStran />}></Route>
         <Route path='/' element={<PrvaStran></PrvaStran>}></Route>
       </Routes>
-
-  </BrowserRouter>
     )
 }
 
