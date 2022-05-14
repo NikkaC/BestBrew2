fe323fedf5fe7f6e71bdfac6dc7e8b3989461f7d
golezdavid <golezdavid@gmail.com>
2022-05-14 18:49:38 +0200

    temaZaPrvoStran

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav} from 'react-bootstrap';
 import NavB from './components/nav';
+import PrvaStran from './components/prvastran';
 
 
 function App() {
   return (
     <div className="App">
 <NavB></NavB>
+<PrvaStran></PrvaStran>
     </div>
     )
 }
--- src/components/prvastran.js
+++ src/components/prvastran.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/prvastran.css';
+import pivo from './BESTBREW.png';
+import Image from 'react-bootstrap/Image'
 
 function PrvaStran() {
   return (
+<>
+<p class=" text-center, fw-bold, glavnoText">BESTBREW<br></br>
+Za vse prave ljublitelje piv.</p>
+<Image src={pivo}></Image>
+</>
 
-<p>Začetek</p>
+
+
   );
 }
 
-export default Navbar;
+export default PrvaStran;
