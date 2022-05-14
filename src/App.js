aedd2dc9008a2e6d1083c6686a27ab80665d54fc
golezdavid <golezdavid@gmail.com>
2022-05-14 17:55:44 +0200

    navbar commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar bg="light">
+    <Container>
+      <Nav className="me-auto">
+      <Navbar.Brand href="#prva">
+          Prva Stran
+      </Navbar.Brand>
+
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        </Nav>
+    </Container>
+    </Navbar>
     </div>
-  );
+    )
 }
 
 export default App;
--- src/components/prvastran.js
+++ src/components/prvastran.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function PrvaStran() {
   return (
 
+<p>Začetek</p>
   );
 }
 
-export default App;
+export default Navbar;
