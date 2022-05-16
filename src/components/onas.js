f601aed992ca482b5585dbac59085cb06bee7156
golezdavid <golezdavid@gmail.com>
2022-05-16 14:10:28 +0200

    updatekomponente

--- src/components/onas.js
+++ src/components/onas.js
@@ -1,46 +1,16 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/onas.css';
+import React from "react";
+import { Card } from "react-bootstrap";
 
 function Onas() {
-  return (
-    <>
-      <Container fluid>
-
-        <p class=" text-center, fw-bold, glavnoText">NEKAJ O NAS<br></br>
-          EKIPA:</p>
-        <Image src={pivo}></Image>
-
-
-        <Card>
-          <Card.Body  style={ {color: "primary"} }>
-            <Card.Text>
-              BESTBREW
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br />
-        <Row>
-          <Col>
-            <Card body>
-
+    return (
+        <>
+            <Card>
+                <Card.Text>
+                    TO SMO MI
+                </Card.Text>
             </Card>
-
-
-          </Col>
-          <Col>
-            <Card body>
-              
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-
-    </>
-
-
-
-  );
+        </>
+    )
 }
 
 export default Onas;
