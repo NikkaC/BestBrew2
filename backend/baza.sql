c4bb5fa9fcfec32069c560077ed92cbe953d0cc0
poetovio <urban.vizintin@outlook.com>
2022-05-17 17:43:52 +0200

    Podatkovna baza -> debugged. Osnovna baza postavljena

--- backend/baza.sql
+++ backend/baza.sql
@@ -1,8 +1,3 @@
-DROP DATABASE IF EXISTS baza;
-
-CREATE DATABASE baza;
-
-DROP TABLE IF EXISTS pivo;
 DROP TABLE IF EXISTS ocena;
 DROP TABLE IF EXISTS priljubljeno_pivo;
 DROP TABLE IF EXISTS seznam_piva;
@@ -11,7 +6,7 @@ DROP TABLE IF EXISTS uporabnik;
 DROP TABLE IF EXISTS pivovarna;
 
 CREATE TABLE pivo (
-	idPivo serial PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	idPivo serial PRIMARY KEY NOT NULL,
     tk_pivovarna INT NOT NULL,
     naziv VARCHAR(45),
     alkohol VARCHAR(45),
@@ -67,38 +62,27 @@ ALTER TABLE pivo ADD CONSTRAINT tk_pivo_pivovarna FOREIGN KEY (tk_pivovarna) REF
 
 
 INSERT INTO uporabnik (ime, priimek, email, geslo, datum_rojstva)
-VALUES  ('Jože', 'Gorišek', "joze.gorisek@gmail.com", "gorisek5", "1975-05-01"),
-        ("Buba", "Corelli", "imperia@gmail.com", "bacamruze", "1989-09-22"),
-        ("Jala", "Brat", "jala.brat@gmail.com", "partijam", "1986-10-16"),
-        ("Alfi", "Nipič", "alfi.nipic@gmail.com", "pohorje", "1944-09-17"),
-        ("Saša", "Lendero", "sasa.lendero@gmail.com", "mandoline", "1973-08-06"),
-        ("Robert", "Pešut", "robert.pesut@gmail.com", "magnifico", "1965-12-01"),
-        ("Tomaž", "Mihelič", "tomaz.mihelic@gmail.com", "toplovodar", "1979-06-21"),
-        ("Jan", "Plestenjak", "jan.plestenjak@gmail.com", "soba102", "1973-03-27"),
-        ("Stevie", "Wonder", "stevie.wonder@gmail.com", "tjema", "1950-05-13"),
-        ("Fredi", "Miler", "fredi.miler@gmail.com", "obrv", "1967-05-22");
+VALUES  ('Jože', 'Gorišek', 'joze.gorisek@gmail.com', 'gorisek5', '1975-05-01'),
+        ('Buba', 'Corelli', 'imperia@gmail.com', 'bacamruze', '1989-09-22'),
+        ('Jala', 'Brat', 'jala.brat@gmail.com', 'partijam', '1986-10-16'),
+        ('Alfi', 'Nipič', 'alfi.nipic@gmail.com', 'pohorje', '1944-09-17'),
+        ('Saša', 'Lendero', 'sasa.lendero@gmail.com', 'mandoline', '1973-08-06'),
+        ('Robert', 'Pešut', 'robert.pesut@gmail.com', 'magnifico', '1965-12-01'),
+        ('Tomaž', 'Mihelič', 'tomaz.mihelic@gmail.com', 'toplovodar', '1979-06-21'),
+        ('Jan', 'Plestenjak', 'jan.plestenjak@gmail.com', 'soba102', '1973-03-27'),
+        ('Stevie', 'Wonder', 'stevie.wonder@gmail.com', 'tjema', '1950-05-13'),
+        ('Fredi', 'Miler', 'fredi.miler@gmail.com', 'obrv', '1967-05-22');
 
 INSERT INTO pivovarna (naziv_pivovarne, x_koordinata, y_koordinata)
-VALUES  ("Union", 430, 377),
-        ("Heineken", 390, 580),
-        ("Laško", 441, 380),
-        ("Staropramen", 478, 499),
-        ("Carlsberg", 444, 555),
-        ("Guinness", 439, 12),
-        ("Green Golden Brewing", 447, 520);
+VALUES  ('Union', 430, 377),
+        ('Heineken', 390, 580),
+        ('Laško', 441, 380),
+        ('Staropramen', 478, 499),
+        ('Carlsberg', 444, 555),
+        ('Guinness', 439, 12),
+        ('Green Golden Brewing', 447, 520);
 
-INSERT INTO seznamPiva VALUES(NULL, 1);
-INSERT INTO seznamPiva VALUES(NULL, 2);
-INSERT INTO seznamPiva VALUES(NULL, 3);
-INSERT INTO seznamPiva VALUES(NULL, 4);
-INSERT INTO seznamPiva VALUES(NULL, 5);
-INSERT INTO seznamPiva VALUES(NULL, 6);
-INSERT INTO seznamPiva VALUES(NULL, 7);
-INSERT INTO seznamPiva VALUES(NULL, 8);
-INSERT INTO seznamPiva VALUES(NULL, 9);
-INSERT INTO seznamPiva VALUES(NULL, 10);
-
-INSERT INTO seznamPiva (tk_uporabnik)
+INSERT INTO seznam_Piva (tk_uporabnik)
 VALUES  (1),
         (2),
         (3),
@@ -110,74 +94,74 @@ VALUES  (1),
         (9),
         (10);
 
-INSERT INTO pivo VALUES (tk_pivovarna, naziv, alkohol, vrsta, pena, okus, vonj, crtna_koda)
-VALUES  (NULL, 1, "Union Nefiltrirano", "alkoholno", "Nefiltrirano", 3, 3, 4, 87267),
-        (NULL, 1, "Union brezalkoholno", "brezalkoholno", "svetlo", 2, 2, 3, 83782),
-        (NULL, 3, "Zlatorog", "alkoholno", "svetlo", 3, 5, 3, 52938),
-        (NULL, 2, "Heineken", "alkoholno", "svetlo", 4, 5, 4, 30293),
-        (NULL, 2, "Heineken 0,0", "brezalkoholno", "svetlo", 2, 3, 4, 38293),
-        (NULL, 4, "Staropramen", "alkoholno", "svetlo", 3, 4, 4, 63948),
-        (NULL, 5, "Carlsberg", "alkoholno" "svetlo", 4, 4, 3, 42938),
-        (NULL, 6, "Guinness", "alkoholno", "temno", 4, 3, 2, 18349),
-        (NULL, 3, "Ipa pivo", "alkoholno", "ipa", 3, 4, 5, 51829),
-        (NULL, 3, "Zlatorog temno", "alkoholno", "temno", 5, 3, 3, 58293),
-        (NULL, 1, "Union", "alkoholno", "svetlo", 4, 4, 3, 89320),
-        (NULL, 1, "Radler grenivka", "alkoholno", "radler", 2, 3, 3, 84410),
-        (NULL, 1, "Smile", "alkoholno", "svetlo", 1, 3, 4, 82304),
-        (NULL, 3, "Temni malt", "brezalkoholno", "malt", 1, 2, 4, 52938),
-        (NULL, 3, "Pšenično pivo", "alkoholno", "pšenično", 3, 3, 3, 52039),
-        (NULL, 3, "Golding", "alkoholno", "svetlo", 3, 5, 4, 56293),
-        (NULL, 3, "Zlatorog 0,0", "brezalkoholno", "svetlo", 2, 3, 2, 59321),
-        (NULL, 6, "Guinness", "alkoholno", "temno", 4, 3, 2, 18349),
-        (NULL, 3, "Ipa pivo", "alkoholno", "ipa", 3, 4, 5, 51829),
-        (NULL, 3, "Zlatorog temno", "alkoholno", "temno", 5, 3, 3, 58293),
-        (NULL, 1, "Union", "alkoholno", "svetlo", 4, 4, 3, 89320),
-        (NULL, 1, "Radler grenivka", "alkoholno", "radler", 2, 3, 3, 84417),
-        (NULL, 1, "Smile", "alkoholno", "svetlo", 1, 3, 4, 82304),
-        (NULL, 3, "Temni malt", "brezalkoholno", "malt", 1, 2, 4, 52938),
-        (NULL, 3, "Pšenično pivo", "alkoholno", "pšenično", 3, 3, 3, 52039),
-        (NULL, 3, "Golding", "alkoholno", "svetlo", 3, 5, 4, 56293),
-        (NULL, 3, "Zlatorog 0,0", "brezalkoholno", "svetlo", 2, 3, 2, 59321),
-        (NULL, 5, "Carlsberg 0,0", "brezalkoholno", "svetlo", 2, 4, 3, 41928),
-        (NULL, 5, "Nordic Ale", "brezalkoholno", "svetlo", 3, 3, 2, 49283),
-        (NULL, 5, "1664 Bio Non Filtrée", "alkoholno", "nefiltrirano", 4, 3, 4, 41829),
-        (NULL, 5, "1664 Créations French Style IPA", "alkoholno", "ipa", 3, 4, 3, 43928),
-        (NULL, 5, "Alivaria Beloe Zoloto", "alkoholno", "pšenično", 4, 3, 4, 43726),
-        (NULL, 5, "Arsenalnoe Non-alcoholic", "brezalkoholno", "svetlo", 2, 2, 3, 48279),
-        (NULL, 4, "Staropramen Dark", "alkoholno", "temno", 5, 3, 4, 69283),
-        (NULL, 4, "Staropramen Premium", "alkoholno", "svetlo", 3, 5, 4, 63293),
-        (NULL, 4, "Staropramen nefiltrirano", "alkoholno", "nefiltrirano", 4, 3, 4, 63534),
-        (NULL, 4, "Staropramen brezalkoholno", "brezalkoholno", "svetlo", 2, 3, 3, 69382),
-        (NULL, 6, "Guinness Draught", "alkoholno", "temno", 3, 4, 5, 19238),
-        (NULL, 6, "Guinness Foreign Extra Scout", "alkoholno", "temno", 4, 3, 4, 17383),
-        (NULL, 6, "Nitro IPA", "alkoholno", "ipa", 3, 4, 5, 19348),
-        (NULL, 6, "Blond American Lager", "alkoholno", "svetlo", 3, 5, 4, 12946),
-        (NULL, 6, "Hop House 13 Lager", "alkoholno", "svetlo", 4, 4, 3, 18364),
-        (NULL, 6, "Golden Ale", "alkoholno", "svetlo", 3, 5, 4, 14832),
-        (NULL, 6, "Dublin Porter", "alkoholno", "svetlo", 4, 3, 3, 13203),
-        (NULL, 2, "Sodček Draught Kreg", "alkoholno", "svetlo", 3, 4, 5, 17826),
-        (NULL, 2, "Extra Cold", "alkoholno", "svetlo", 3, 5, 5, 13495),
-        (NULL, 7, "Rocket Queen", "alkoholno", "ipa", 4, 4, 3, 28447),
-        (NULL, 7, "Fear and Loathing in Belgrade", "alkoholno", "ipa", 3, 3, 5, 29384),
-        (NULL, 7, "Wolf Pack", "alkoholno", "svetlo", 4, 4, 2, 27493),
-        (NULL, 7, "Best Buds", "alkoholno", "svetlo", 3, 3, 4, 29484),
-        (NULL, 7, "Drargon Slayer", "alkoholno", "ipa", 3, 4, 5, 22837),
-        (NULL, 7, "Emergency Landing", "alkoholno", "neipa", 4, 3, 5, 21928),
-        (NULL, 7, "Povodni mož", "alkoholno", "ipa", 2, 5, 3, 28374),
-        (NULL, 7, "Rocket Queen", "alkoholno", "ipa", 4, 4, 3, 28447),
-        (NULL, 7, "Fear and Loathing in Belgrade", "alkoholno", "ipa", 3, 3, 5, 29384),
-        (NULL, 7, "Wolf Pack", "alkoholno", "svetlo", 4, 4, 2, 27493),
-        (NULL, 7, "Best Buds", "alkoholno", "svetlo", 3, 3, 4, 29484),
-        (NULL, 7, "Drargon Slayer", "alkoholno", "ipa", 3, 4, 5, 22837),
-        (NULL, 7, "Emergency Landing", "alkoholno", "neipa", 4, 3, 5, 21928),
-        (NULL, 7, "Povodni mož", "alkoholno", "ipa", 2, 5, 3, 28374),
-        (NULL, 7, "Cashmere", "alkoholno", "neipa", 3, 4, 2, 21239),
-        (NULL, 7, "Sultans of Hops", "alkoholno", "ipa", 4, 3, 5, 23948),
-        (NULL, 7, "Forbidden fruit", "alkoholno", "svetlo", 3, 4, 4, 29398),
-        (NULL, 7, "Komb4jn", "alkoholno", "svetlo", 3, 3, 4, 27368),
-        (NULL, 7, "Papagena", "alkoholno", "ipa", 5, 2, 3, 28347),
-        (NULL, 7, "Oli Warz", "alkoholno", "temno", 5, 3, 3, 26384),
-        (NULL, 7, "Franci", "alkoholno", "svetlo", 4, 4, 3, 28464);
+INSERT INTO pivo (tk_pivovarna, naziv, alkohol, vrsta, pena, okus, vonj, crtna_koda)
+VALUES  (1, 'Union Nefiltrirano', 'alkoholno', 'nefiltrirano', 3, 3, 4, 87267),
+        (1, 'Union brezalkoholno', 'brezalkoholno', 'svetlo', 2, 2, 3, 83782),
+        (3, 'Zlatorog', 'alkoholno', 'svetlo', 3, 5, 3, 52938),
+        (2, 'Heineken', 'alkoholno', 'svetlo', 4, 5, 4, 30293),
+        (2, 'Heineken 0.0', 'brezalkoholno', 'svetlo', 2, 3, 4, 38293),
+        (4, 'Staropramen', 'alkoholno', 'svetlo', 3, 4, 4, 63948),
+        (5, 'Carlsberg', 'alkoholno', 'svetlo', 4, 4, 3, 42938),
+        (6, 'Guinness', 'alkoholno', 'temno', 4, 3, 2, 18349),
+        (3, 'Ipa pivo', 'alkoholno', 'ipa', 3, 4, 5, 51829),
+        (3, 'Zlatorog temno', 'alkoholno', 'temno', 5, 3, 3, 58293),
+        (1, 'Union', 'alkoholno', 'svetlo', 4, 4, 3, 89320),
+        (1, 'Radler grenivka', 'alkoholno', 'radler', 2, 3, 3, 84410),
+        (1, 'Smile', 'alkoholno', 'svetlo', 1, 3, 4, 82304),
+        (3, 'Temni malt', 'brezalkoholno', 'malt', 1, 2, 4, 52938),
+        (3, 'Pšenično pivo', 'alkoholno', 'pšenično', 3, 3, 3, 52039),
+        (3, 'Golding', 'alkoholno', 'svetlo', 3, 5, 4, 56293),
+        (3, 'Zlatorog 0.0', 'brezalkoholno', 'svetlo', 2, 3, 2, 59321),
+        (6, 'Guinness', 'alkoholno', 'temno', 4, 3, 2, 18349),
+        (3, 'Ipa pivo', 'alkoholno', 'ipa', 3, 4, 5, 51829),
+        (3, 'Zlatorog temno', 'alkoholno', 'temno', 5, 3, 3, 58293),
+        (1, 'Union', 'alkoholno', 'svetlo', 4, 4, 3, 89320),
+        (1, 'Radler grenivka', 'alkoholno', 'radler', 2, 3, 3, 84417),
+        (1, 'Smile', 'alkoholno', 'svetlo', 1, 3, 4, 82304),
+        (3, 'Temni malt', 'brezalkoholno', 'malt', 1, 2, 4, 52938),
+        (3, 'Pšenično pivo', 'alkoholno', 'pšenično', 3, 3, 3, 52039),
+        (3, 'Golding', 'alkoholno', 'svetlo', 3, 5, 4, 56293),
+        (3, 'Zlatorog 0.0', 'brezalkoholno', 'svetlo', 2, 3, 2, 59321),
+        (5, 'Carlsberg 0.0', 'brezalkoholno', 'svetlo', 2, 4, 3, 41928),
+        (5, 'Nordic Ale', 'brezalkoholno', 'svetlo', 3, 3, 2, 49283),
+        (5, '1664 Bio Non Filtrée', 'alkoholno', 'nefiltrirano', 4, 3, 4, 41829),
+        (5, '1664 Créations French Style IPA', 'alkoholno', 'ipa', 3, 4, 3, 43928),
+        (5, 'Alivaria Beloe Zoloto', 'alkoholno', 'pšenično', 4, 3, 4, 43726),
+        (5, 'Arsenalnoe Non-alcoholic', 'brezalkoholno', 'svetlo', 2, 2, 3, 48279),
+        (4, 'Staropramen Dark', 'alkoholno', 'temno', 5, 3, 4, 69283),
+        (4, 'Staropramen Premium', 'alkoholno', 'svetlo', 3, 5, 4, 63293),
+        (4, 'Staropramen nefiltrirano', 'alkoholno', 'nefiltrirano', 4, 3, 4, 63534),
+        (4, 'Staropramen brezalkoholno', 'brezalkoholno', 'svetlo', 2, 3, 3, 69382),
+        (6, 'Guinness Draught', 'alkoholno', 'temno', 3, 4, 5, 19238),
+        (6, 'Guinness Foreign Extra Scout', 'alkoholno', 'temno', 4, 3, 4, 17383),
+        (6, 'Nitro IPA', 'alkoholno', 'ipa', 3, 4, 5, 19348),
+        (6, 'Blond American Lager', 'alkoholno', 'svetlo', 3, 5, 4, 12946),
+        (6, 'Hop House 13 Lager', 'alkoholno', 'svetlo', 4, 4, 3, 18364),
+        (6, 'Golden Ale', 'alkoholno', 'svetlo', 3, 5, 4, 14832),
+        (6, 'Dublin Porter', 'alkoholno', 'svetlo', 4, 3, 3, 13203),
+        (2, 'Sodček Draught Kreg', 'alkoholno', 'svetlo', 3, 4, 5, 17826),
+        (2, 'Extra Cold', 'alkoholno', 'svetlo', 3, 5, 5, 13495),
+        (7, 'Rocket Queen', 'alkoholno', 'ipa', 4, 4, 3, 28447),
+        (7, 'Fear and Loathing in Belgrade', 'alkoholno', 'ipa', 3, 3, 5, 29384),
+        (7, 'Wolf Pack', 'alkoholno', 'svetlo', 4, 4, 2, 27493),
+        (7, 'Best Buds', 'alkoholno', 'svetlo', 3, 3, 4, 29484),
+        (7, 'Drargon Slayer', 'alkoholno', 'ipa', 3, 4, 5, 22837),
+        (7, 'Emergency Landing', 'alkoholno', 'neipa', 4, 3, 5, 21928),
+        (7, 'Povodni mož', 'alkoholno', 'ipa', 2, 5, 3, 28374),
+        (7, 'Rocket Queen', 'alkoholno', 'ipa', 4, 4, 3, 28447),
+        (7, 'Fear and Loathing in Belgrade', 'alkoholno', 'ipa', 3, 3, 5, 29384),
+        (7, 'Wolf Pack', 'alkoholno', 'svetlo', 4, 4, 2, 27493),
+        (7, 'Best Buds', 'alkoholno', 'svetlo', 3, 3, 4, 29484),
+        (7, 'Drargon Slayer', 'alkoholno', 'ipa', 3, 4, 5, 22837),
+        (7, 'Emergency Landing', 'alkoholno', 'neipa', 4, 3, 5, 21928),
+        (7, 'Povodni mož', 'alkoholno', 'ipa', 2, 5, 3, 28374),
+        (7, 'Cashmere', 'alkoholno', 'neipa', 3, 4, 2, 21239),
+        (7, 'Sultans of Hops', 'alkoholno', 'ipa', 4, 3, 5, 23948),
+        (7, 'Forbidden fruit', 'alkoholno', 'svetlo', 3, 4, 4, 29398),
+        (7, 'Komb4jn', 'alkoholno', 'svetlo', 3, 3, 4, 27368),
+        (7, 'Papagena', 'alkoholno', 'ipa', 5, 2, 3, 28347),
+        (7, 'Oli Warz', 'alkoholno', 'temno', 5, 3, 3, 26384),
+        (7, 'Franci', 'alkoholno', 'svetlo', 4, 4, 3, 28464);
 
 INSERT INTO ocena (tk_pivo, tk_uporabnik, vrednost)
 VALUES  (2, 4, 1),
