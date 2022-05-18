89465c52bea3a74ef9bbeea79610a98fdc2f0c0c
maticabsec <matic.absec@student.um.si>
2022-05-18 17:29:26 +0200

    /VsaPiva in /VsiUporabniki backend

---
+++ backend/UstavriPostgresTabele.js
@@ -0,0 +1,39 @@
+/*
+  Terminal -> New Terminal
+  node backend/index.js 
+*/
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;  
+const fs = require('fs');
+const path = require('path');
+const Pool = require("pg").Pool;
+
+//--------CONFIG ZA POVEZAVO PREKO URI-------
+const proConfig = {
+  connectionString: "postgres://jzzmbybrrlfzvf:eb31bfd4a0b6b53a56f7b480ccf28b4aa4fda39475fe1765df64e03c33f5bc8d@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/deqj5mqahdj49p", //geslo,username,database,user
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+const pool = new Pool(proConfig);
+
+var sqlPoizvedba = fs.readFileSync(__dirname +'/baza.sql').toString();
+
+//---Testna POVEZAVA in IZVRŠITEV baza.sql----
+
+pool.connect(function(err, client, done){
+    if(err){
+        console.log('error: ', err);
+        process.exit(1);
+    }
+    client.query(sqlPoizvedba, function(err, result){
+        done();
+        if(err){
+            console.log('error: ', err);
+            process.exit(1);
+        }
+        process.exit(0);
+    });
+});
--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;  
 const fs = require('fs');
 const path = require('path');
+const bodyParser=require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 const Pool = require("pg").Pool;
 
 //--------CONFIG ZA POVEZAVO PREKO URI-------
@@ -20,26 +23,6 @@ const proConfig = {
 //-------------Spremenljivka-----------------
 const pool = new Pool(proConfig);
 
-var sqlPoizvedba = fs.readFileSync(__dirname +'/baza.sql').toString();
-
-//---Testna POVEZAVA in IZVRŠITEV baza.sql----
-
-pool.connect(function(err, client, done){
-    if(err){
-        console.log('error: ', err);
-        process.exit(1);
-    }
-    client.query(sqlPoizvedba, function(err, result){
-        done();
-        if(err){
-            console.log('error: ', err);
-            process.exit(1);
-        }
-        process.exit(0);
-    });
-});
-
-
 app.use(cors());
 
 //----------test na portu localhost:5000/test-----------
@@ -47,13 +30,45 @@ app.get("/test", async (req, res) => {
     res.send({ express: 'Test Backenda' });
 });
 
-app.get("/piva", async (req, res) => {
+app.get("/Vsapiva", async (req, res) => {
     try {
       const vsaPiva = await pool.query("SELECT * FROM pivo");
       res.json(vsaPiva.rows);
     } catch (err) {
       console.error(err.message);
     }
+});
+
+app.get("/VseUporabnike", async (req, res) => {
+  try {
+    const visUporabniki = await pool.query("SELECT * FROM uporabnik");
+    res.json(visUporabniki.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+app.post("/dodajUporabnika", async (req, res) => {
+  const {ime,priimek,email,geslo,datum_rojstva} = req.body
+
+  pool.query('INSERT INTO uporabnik (&1, &2, &3, &4, &5)', [ ime, priimek,email,geslo,datum_rojstva], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`Uporabnik added with ID: ${results.insertId}`)
+  })
+});
+
+app.post("/dodajPivo", async (req, res) => {
+  const {naziv, alkohol,vrsta,pena,okus,vonj,crtna_koda } = req.body
+
+  pool.query('INSERT INTO pivo (&1, &2, &3, &4, &5, &6, &7)', [ naziv, alkohol,vrsta,pena,okus,vonj,crtna_koda], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`Pivo added with ID: ${results.insertId}`)
+  })
 });
 
 app.listen(5000, () => {
