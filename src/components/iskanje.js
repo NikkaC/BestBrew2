340773215fcfdc177f9f3994cd9c556cf058b9ad
maticabsec <matic.absec@student.um.si>
2022-05-21 01:09:55 +0200

    seznamPiv.js dodan v iskanje.js

--- src/components/iskanje.js
+++ src/components/iskanje.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button,Container } from "react-bootstrap";
 import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
 import '../styles/iskanje.css';
 import 'mdbreact/dist/css/mdb.css'
+import SeznamPiv from "./seznamPiv";
 
 function Iskanje() {
     return (
-        <>
+    <>
         <center>
-            <Card>
-                <Card.Text>
-                <div>
-      <input class="searchBar" placeholder="Vnesi ime piva"/>
-      <Button size="lg"  variant="secondary">Poišči pivo</Button>{' '}
-    </div>
-                </Card.Text>
-            </Card>
-            </center>
-        </>
+                <Card>
+                    <Card.Text>
+                        <div>
+                            <input class="searchBar" placeholder="Vnesi ime piva"/>
+                            <Button size="lg"  variant="secondary">Poišči pivo</Button>{' '}
+                        </div>
+                    </Card.Text>
+                </Card>
+        </center>
+        <Container>  
+            <SeznamPiv/>
+        </Container>    
+    </>
     )
 }
 
---
+++ src/components/seznamPiv.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, ListGroup, Button,CardGroup } from "react-bootstrap";
+import axios from 'axios';
+
+export default class SeznamPiv extends React.Component {
+  state = {
+    piva: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/Vsapiva`)
+      .then(res => {
+        const piva = res.data;
+        this.setState({ piva });
+      })
+  }
+
+  render() {
+    return (
+        <CardGroup>
+        {
+          this.state.piva
+            .map(pivo =>
+                <Card className="flex-fill m-1" style={{ width: '12rem' }}>
+                    <Card.Body>
+                        <Card.Title>{pivo.naziv}</Card.Title>
+                        <Card.Subtitle>{pivo.naziv_pivovarne}</Card.Subtitle>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>{pivo.alkohol}</ListGroup.Item>
+                            <ListGroup.Item>{pivo.vrsta}</ListGroup.Item>
+                            <ListGroup.Item>Pena: {pivo.pena}</ListGroup.Item>
+                            <ListGroup.Item>Okus: {pivo.okus}</ListGroup.Item>
+                            <ListGroup.Item>Vonj: {pivo.vonj}</ListGroup.Item>
+                        </ListGroup>
+                        <Button variant="primary">Več</Button>
+                    </Card.Body>
+                </Card>
+            )
+        }
+      </CardGroup>
+    )
+  }
+}
