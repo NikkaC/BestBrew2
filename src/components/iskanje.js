b33a599ea5d41636921f11762b81040e4e9d8a6d
golezdavid <golezdavid@gmail.com>
2022-05-24 20:32:57 +0200

    Search bar (deluje samo za svetlost)

--- src/components/iskanje.js
+++ src/components/iskanje.js
@@ -1,28 +1,73 @@
-import React from "react";
-import { Card, Button,Container } from "react-bootstrap";
-import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
+import React, { useState } from "react";
+import { Card, Button, Container, ListGroup, CardGroup } from "react-bootstrap";
 import '../styles/iskanje.css';
 import 'mdbreact/dist/css/mdb.css'
 import SeznamPiv from "./seznamPiv";
+import axios from 'axios';
+var Barcode = require('react-barcode');
 
-function Iskanje() {
+
+
+export default class Iskanje extends React.Component {
+
+  state = {
+    piva: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5001/Vsapiva`)
+      .then(res => {
+        const piva = res.data;
+        this.setState({ piva });
+      })
+  }
+
+
+
+  searchChanged = event => {
+    this.setState({ search: event.target.value })
+  }
+
+  render() {
     return (
-    <>
-        <center>
-                <Card>
-                    <Card.Text>
-                        <div>
-                            <input class="searchBar" placeholder="Vnesi ime piva"/>
-                            <Button size="lg"  variant="secondary">Poišči pivo</Button>{' '}
-                        </div>
-                    </Card.Text>
-                </Card>
-        </center>
-        <Container>  
-            <SeznamPiv/>
-        </Container>    
-    </>
-    )
-}
+      <>
 
-export default Iskanje;
+        <input type='text' placeholder="Išči po svetlosti piva" onChange={this.searchChanged} value={this.state.search} />
+        <CardGroup>
+          {this.state.piva
+            .filter(pivo => pivo.vrsta.includes(this.state.search))
+            .map(pivo => (
+              <>
+              
+                <Card className="flex-fill m-1" style={{ width: '12rem' }}>
+                    <Card.Body>
+                        <Card.Title>{pivo.naziv}</Card.Title>
+                        <Card.Subtitle>{pivo.naziv_pivovarne}</Card.Subtitle>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>{pivo.alkohol}</ListGroup.Item>
+                            <ListGroup.Item>{pivo.vrsta}</ListGroup.Item>
+                            <ListGroup.Item>Pena: {pivo.pena}</ListGroup.Item>
+                            <ListGroup.Item>Okus: {pivo.okus}</ListGroup.Item>
+                            <ListGroup.Item>Vonj: {pivo.vonj}</ListGroup.Item>
+                            <Barcode value={pivo.crtna_koda} />,
+                        </ListGroup>
+                        <Button variant="primary">Več</Button>
+                    </Card.Body>
+                </Card>
+              </>
+            )
+            )}
+            </CardGroup>
+        <center>
+          <Card>
+            <Card.Text>
+
+            </Card.Text>
+          </Card>
+        </center>
+        <Container>
+        </Container>
+      </>
+    )
+  }
+}
