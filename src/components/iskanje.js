162abca1ee740baf0f633d7b2272735f26e1c3e7
maticabsec <matic.absec@student.um.si>
2022-05-28 16:01:58 +0200

    2.5 pipline

--- src/components/iskanje.js
+++ src/components/iskanje.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import ReactDOM from 'react-dom';
-import { StrictMode } from "react";
-import { Card, Button,Container } from "react-bootstrap";
-import '../styles/iskanje.css';
-import 'mdbreact/dist/css/mdb.css'
-import SeznamPiv from "./seznamPiv";
-import Modal from "./modal.js";
-
-class Iskanje extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            show: false
-        };
-        this.prikazi = this.prikazi.bind(this);
-        this.skrij = this.skrij.bind(this);
-    }
-
-    prikazi = () => {
-        this.setState({ show: true });
-    }
-
-    skrij = () => {
-        this.setState({ show: false });
-    }
-
-    render() {
-
-        return (
-        <>
-            <center>
-                    <Card>
-                        <Card.Text>
-                            <div>
-                                <input class="searchBar" placeholder="Vnesi ime piva"/>
-                                <Button size="lg"  variant="secondary">Poišči pivo</Button>{' '}
-                                <Button size="lg" onClick={this.prikazi} variant="secondary">Skeniraj kodo</Button>{' '}
-                            </div>
-                        </Card.Text>
-                    </Card>
-            </center>
-
-            <Modal show = {this.state.show} handleClose = {this.skrij}>
-                <h1>Iskanje ~ barcode</h1>
-            </Modal>
-
-            <Container>
-                <SeznamPiv/>
-            </Container>    
-        </>
-        );
-    }
-=======
 import React, { useState, useEffect } from "react";
 import { Card, Button, Container, ListGroup, CardGroup } from "react-bootstrap";
 import '../styles/iskanje.css';
@@ -69,7 +13,6 @@ const handleOnSearch = (string, results) => {
   // onSearch will have as the first callback parameter
   // the string searched and for the second the results.
   console.log(string, results)
->>>>>>> 935b08f04eee6e08816c03dbb7bb7d73ffce125d
 }
 
 
