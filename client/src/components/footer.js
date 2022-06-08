52d326bd854a34f423cc184c21662b1a11fd5835
NikkaC <nik.kac@student.um.si>
2022-06-08 16:24:15 +0200

    Push

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { Link } from "react-router-dom";
+
 
 const Footer = () => {
   return (
     <MDBFooter color="#bbb">
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="./onas">Ekipa BestBrew </a>
+          &copy; {new Date().getFullYear()} Copyright: <Link to="/onas"> <a>Ekipa BestBrew </a></Link>
         </MDBContainer>
       </div>
     </MDBFooter>
